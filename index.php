d826e92eac8c835c58dea6c301c1c807584197ce
lennya0404 <lwiesinger74@gmail.com>
2025-04-08 10:26:23 +0200

    php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Flashcard</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <header>
+      <h1>⚡️ Flashcard</h1>
+    </header>
+
+    <main>
+      <section class="text">
+        <h2>SAVORY WHEELS</h2>
+        <h3>Zeal Taste:<br />Culinary Delights on Wheels</h3>
+        <p>
+          Quizlet ist eine Lernplattform, mit der du Vokabeln, Begriffe oder
+          Fakten lernen kannst. Du erstellst digitale Karteikarten (Flashcards)
+          mit Fragen und Antworten. Dann kannst du mit verschiedenen Spielen und
+          Tests üben – alleine oder mit anderen.
+        </p>
+        <a href="#" class="button">Book Your Table</a>
+      </section>
+
+      <section class="image">
+        <img src="foodtruck.jpg" alt="Food Truck" />
+      </section>
+    </main>
+  </body>
+</html>
