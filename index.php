4b1c9dfbc2d24ecd9e3e37440f28aec780380155
lennya0404 <lwiesinger74@gmail.com>
2025-04-09 08:28:40 +0200

    image

--- index.php
+++ index.php
@@ -3,17 +3,21 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+
   <title>Document</title>
 </head>
 <body>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.min.js" integrity="sha384-VQqxDN0EQCkWoxt/0vsQvZswzTHUVOImccYmSyhJTp7kGtPed0Qcx8rK9h9YEgx+" crossorigin="anonymous"></script>
 <header>
     <h1>⚡️Flashcards</h1>
   </header>
   <main class="tx">
     <div class="tx-text">
-      <p class="category">SAVORY WHEELS</p>
-      <h1>Zeal Taste: <br> Culinary Delights on Wheels</h1>
-      <p class="subtitle">Bringing Gourmet Street Food to Your Neighborhood –<br> Fresh, Flavorful, and Fast!</p>
+      <h1> Was ist Flashcards</h1>
+      
+
       <a href="#" class="cta-button">Book Your Table</a>
     </div>
     <div class="tx-image">
