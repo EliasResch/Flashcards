b66eddba782742346b50d7d152b839a5e421cdfd
lennya0404 <lwiesinger74@gmail.com>
2025-04-09 08:17:51 +0200

    verbsserung

--- index.php
+++ index.php
@@ -1,35 +1,26 @@
 <!DOCTYPE html>
-<html lang="de">
-  <head>
-    <meta charset="UTF-8" />
-    <title>Flashcard</title>
-    <link rel="stylesheet" href="style.css" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<header>
+    <h1>⚡️Flashcards</h1>
+  </header>
+  <main class="tx">
+    <div class="tx-text">
+      <p class="category">SAVORY WHEELS</p>
+      <h1>Zeal Taste: <br> Culinary Delights on Wheels</h1>
+      <p class="subtitle">Bringing Gourmet Street Food to Your Neighborhood –<br> Fresh, Flavorful, and Fast!</p>
+      <a href="#" class="cta-button">Book Your Table</a>
+    </div>
+    <div class="tx-image">
+      <img src="foodtruck.jpg" alt="Food Truck">
+    </div>
+  </main>
 
-  </head>
-  <body>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.min.js" integrity="sha384-VQqxDN0EQCkWoxt/0vsQvZswzTHUVOImccYmSyhJTp7kGtPed0Qcx8rK9h9YEgx+" crossorigin="anonymous"></script>
-    <header>
-      <h1>⚡️ Flashcard</h1>
-    </header>
-
-    <main>
-      <section class="text">
-        <h2>SAVORY WHEELS</h2>
-        <h3>Zeal Taste:<br />Culinary Delights on Wheels</h3>
-        <p>
-          Quizlet ist eine Lernplattform, mit der du Vokabeln, Begriffe oder
-          Fakten lernen kannst. Du erstellst digitale Karteikarten (Flashcards)
-          mit Fragen und Antworten. Dann kannst du mit verschiedenen Spielen und
-          Tests üben – alleine oder mit anderen.
-        </p>
-        <a href="#" class="button">Book Your Table</a>
-      </section>
-
-      <section class="image">
-        <img src="foodtruck.jpg" alt="Food Truck" />
-      </section>
-    </main>
-  </body>
+  
+</body>
 </html>
