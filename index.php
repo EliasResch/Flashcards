6e86bead4ee01f8f94f976549f6c763da6427b49
lennya0404 <lwiesinger74@gmail.com>
2025-04-09 08:34:14 +0200

    .

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
 
   <title>Document</title>
@@ -18,10 +19,10 @@
       <h1> Was ist Flashcards</h1>
       
 
-      <a href="#" class="cta-button">Book Your Table</a>
+      <a href="#" class="cta-button">Eigene Karten erstellen</a>
     </div>
     <div class="tx-image">
-      <img src="foodtruck.jpg" alt="Food Truck">
+      
     </div>
   </main>
 
