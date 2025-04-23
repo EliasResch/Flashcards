6e64049885beff43609d5f34faf221f31a4ae15b
EliasResch <168538473+EliasResch@users.noreply.github.com>
2025-04-23 08:42:04 +0200

    Datenbankverbindung

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+// Datenbankverbindung
+
+$conn = new mysqli("localhost", "root", "", "karteikarten");
+
+// Verbindung prüfen
+if ($conn->connect_error) {
+    die("Verbindung fehlgeschlagen: " . $conn->connect_error);
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,7 +15,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwq+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
   <link rel="stylesheet" href="style.css">
 
   <title>Flashcards</title>
@@ -51,10 +61,4 @@
 </footer>
 
 </body>
-</html>
-
-
-
-</body>
-
 </html>
