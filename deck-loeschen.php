6ca40446a71d06a9c77cfa9a0f1d7f4e506d9eed
EliasResch <elias.resch@tfs-haslach.at>
2025-06-15 21:21:55 +0200

    -

--- deck-loeschen.php
+++ deck-loeschen.php
@@ -10,15 +10,15 @@
         die("Verbindung zur Datenbank fehlgeschlagen: " . $conn->connect_error);
     }
 
-    //Löschen eines Decks
+    // Verarbeitung des POST-Requests zum Löschen eines Decks
     if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['deck_name'])) {
-       
+        // Bereinigen des Deck-Namens und Erstellen des Tabellennamens
         $deck_name = preg_replace('/[^a-zA-Z0-9_]/', '', $_POST['deck_name']);
         $table_name = "deck_" . $deck_name;
 
-        
+        // SQL-Befehl zum Löschen der Tabelle (des Decks)
         $sql = "DROP TABLE IF EXISTS $table_name";
-        
+        // Ausführen des Befehls und Weiterleitung zur Startseite bei Erfolg
         if ($conn->query($sql) === TRUE) {
             header("Location: index.php");
             exit();
@@ -27,7 +27,7 @@
         }
     }
 
-    
+    // Schließen der Datenbankverbindung
     $conn->close();
     ?>
 </body>
